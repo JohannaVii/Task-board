d1adcbb3acfbd5a502ed09c39f5724735b5d8225
Johannavii <jh.viippa@gmail.com>
2026-09-13 10:03:07 +0200

    Uppdaterat backend med persistent filhantering

--- backend/src/db/tasks.js
+++ backend/src/db/tasks.js
@@ -1,1 +1,16 @@
-export const tasks = [];
+import fs from "node:fs";
+
+const filePath = "./src/db/tasks.json";
+
+export const getTasks = () => {
+  try {
+    const data = fs.readFileSync(filePath, "utf8");
+    return JSON.parse(data);
+  } catch (err) {
+    return [];
+  }
+};
+
+export const saveTasks = (tasks) => {
+  fs.writeFileSync(filePath, JSON.stringify(tasks, null, 2), "utf8");
+};
--- backend/src/routes/tasks.js
+++ backend/src/routes/tasks.js
@@ -1,15 +1,20 @@
 import express from "express";
-import { tasks } from "../db/tasks.js";
+import { getTasks, saveTasks } from "../db/tasks.js";
 
 const router = express.Router();
 
 router.get("/", (request, response) => {
+  const tasks = getTasks();
   response.json(tasks);
 });
 
 router.post("/", (request, response) => {
+  const tasks = getTasks();
   const newTask = request.body;
+
   tasks.push(newTask);
+  saveTasks(tasks);
+
   response.status(201).json(newTask);
 });
 
